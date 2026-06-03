ae6f09ec7ae8caecee008bb4d4d18a7a820e067b
Astuti Kumari <kumariastuti258@gmail.com>
2026-06-03 22:48:19 +0530

    Create 3june.java

---
+++ 3june.java
@@ -0,0 +1,80 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            int left=0;
+            int right=n-1;
+            while(left<right){
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+    }
+}
+
+import java.util.*;
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return a[0] - b[0];
+            }
+        });
+
+        List<int[]> ans = new ArrayList<>();
+
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+
+        for(int i = 1; i < intervals.length; i++) {
+
+            if(intervals[i][0] <= end) {
+                end = Math.max(end, intervals[i][1]);
+            }
+            else {
+                ans.add(new int[]{start, end});
+
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+        }
+
+        ans.add(new int[]{start, end});
+
+        return ans.toArray(new int[ans.size()][]);
+    }
+}
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+}
