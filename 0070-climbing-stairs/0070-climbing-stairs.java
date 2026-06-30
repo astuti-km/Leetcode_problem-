5054c405886aa1170c8f49c819d8f2e731197163
Astuti Kumari <kumariastuti258@gmail.com>
2026-06-30 22:27:49 +0530

    Time: 0 ms (100%), Space: 41.9 MB (72.03%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    int[] dp;
+
+    public int climbStairs(int n) {
+        dp = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            dp[i] = -1;
+        }
+
+        return solve(n);
+    }
+
+    public int solve(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+
+        dp[n] = solve(n - 1) + solve(n - 2);
+
+        return dp[n];
+    }
+}
