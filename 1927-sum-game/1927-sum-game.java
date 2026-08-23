cbd265b197ff15256b1811819f28ea1db91b7c1e
Astuti Kumari <kumariastuti258@gmail.com>
2026-08-23 21:43:56 +0530

    Time: 7 ms (79.56%), Space: 46.9 MB (83.94%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int left=0;
+        int right=0;
+
+        int leftQ =0;
+        int rightQ=0;
+
+        for(int i=0;i<n/2;i++){
+            if(num.charAt(i)=='?'){
+                leftQ++;
+            }
+            else {
+                left+=num.charAt(i)-'0';
+            }
+        }
+        for(int i=n/2;i<n;i++){
+            if(num.charAt(i)=='?'){
+                rightQ++;
+            }
+            else {
+                right+=num.charAt(i)-'0';
+            }
+        }
+        int sumdiff=left-right;
+        int diff =rightQ-leftQ;
+
+        return sumdiff*2!=diff*9;
+    }
+}
